adeb7b95498f04f0cc34c5fdec1a974ce4e14425
Nithin2314 <nithin0626@gmail.com>
2023-02-16 16:28:21 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View, Alert, Pressable,onPress } from 'react-native';
+import Loginscreen from './loginpage';
+export default function App(){
+
+  return (<View>
+    <Loginscreen/>
+  </View>)
+}
+
