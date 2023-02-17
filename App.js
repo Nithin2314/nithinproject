2f43ce07d7c2b0152996f35a5d4ea554a523072e
Nithin2314 <nithin0626@gmail.com>
2023-02-17 09:08:06 +0530

    Screens commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, TextInput, View, Alert, Pressable,onPress } from 'react-native';
-import Loginpage from './loginscreen';
-import Signuppage from './signupscreen';
+import Loginpage from './screens/loginscreen';
+import Signuppage from './screens/signupscreen';
 export default function App(){
 
   return (
